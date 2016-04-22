df6c9b951c667b2693fbe7c0aa6014f627f97a34
Pillys <pillys@163.com>
2016-04-22 15:15:03 +0800

    v0.1.0

---
+++ README.md
@@ -0,0 +1,29 @@
+# file loader for webpack
+
+## Usage
+
+[Documentation: Using loaders](http://webpack.github.io/docs/using-loaders.html)
+
+``` javascript
+loaders: [{
+  test: /\.(jpg|jpeg|gif|swf|png|eot|woff|ttf|svg)$/,
+  loader: 'file-loader',
+    query: {
+      context: 'some dir',
+      name: '[path][name].[ext]',
+      map: function(url) {
+        return url;
+      }
+    }
+  }...
+```
+
+By default the file-loader cann't support a filter for the url, this loader can set a function how to fix the url.
+
+## More
+
+see https://github.com/webpack/file-loader
+
+## License
+
+MIT (http://www.opensource.org/licenses/mit-license.php)
---
+++ index.js
@@ -0,0 +1,24 @@
+/*
+  MIT License http://www.opensource.org/licenses/mit-license.php
+  Author Tobias Koppers @sokra
+*/
+var loaderUtils = require('loader-utils');
+module.exports = function(content) {
+  this.cacheable && this.cacheable();
+  if(!this.emitFile) throw new Error('emitFile is required from module system');
+  var queryPlus = this.options.module.loaders.filter(function(v) {
+    return v.loader === 'file-loader-plus';
+  })[0].query;
+  var url = loaderUtils.interpolateName(this, queryPlus.name || '[hash].[ext]', {
+    context: queryPlus.context || this.options.context,
+    content: content,
+    regExp: queryPlus.regExp
+  });
+  if(queryPlus.map) {
+    url = queryPlus.map(url);
+  }
+  this.emitFile(url, content);
+  return 'module.exports = __webpack_public_path__ + ' + JSON.stringify(url) + ';';
+};
+
+module.exports.raw = true;
